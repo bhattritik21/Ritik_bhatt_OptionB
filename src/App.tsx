042fc0d96151ead7ca4eaf2c40e7a5a07b096caa
bhattritik21 <62047462+bhattritik21@users.noreply.github.com>
2024-04-05 11:39:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './App.css';
 import Main from './components/Main';
 import Side from './components/Side';
+import { json } from 'stream/consumers';
+
+import jsondata2 from './components/jsondata2.json';
 
 function App() {
+  const data:any = JSON.stringify(jsondata2)
+  const [jsonData,setjsonData] = useState<string>(data);
   return (
     <div className="App flex p-5">
-      <Main/>
-      <Side/>
+      <Main {...{jsonData}}/>
+      <Side {...{jsonData,setjsonData}}/>
     </div>
   );
 }
